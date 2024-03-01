140ced2e33ed67451f803bc1ff87e3add0f6113c
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-03-01 11:49:19 +0500

    font

--- app/blog/home/layout.tsx
+++ app/blog/home/layout.tsx
@@ -5,7 +5,7 @@ const HomeLayout = ({
   }: Readonly<{
     children: React.ReactNode;
   }>) => {
-    return <main className="container sm:mx-auto">
+    return <main className="container sm:mx-auto inter-font">
         {children}
     </main>
 };
--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <div className={ "container min-h-screen max-w-screen-xl mx-auto"}>
+      <div className={ "container min-h-screen max-w-screen-xl mx-auto inter-font"}>
         <Navbar />
         <div className="grid-home max-w-screen-xl mx-auto gap-4 mt-20">
         <div className="side_bar w-14- transition duration-300 ease-in-out relative">
