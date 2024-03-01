33b94ce7a1b438d34b0302df755ac31de65b3b96
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-03-01 11:28:28 +0500

    Update page.tsx

--- app/blog/home/page.tsx
+++ app/blog/home/page.tsx
@@ -1,9 +1,7 @@
 import BlogHome from "@/components/Home";
 import Navbar from "@/components/Nav";
-interface TestComponentProps {
-  children?: React.ReactNode;
-}
-const Home = (props: TestComponentProps) => {
+
+const Home = () => {
   return (
     <main>
       <BlogHome />
